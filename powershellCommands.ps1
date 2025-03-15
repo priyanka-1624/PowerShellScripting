14d1aca869d33c87b34d00d83f63a446b3037be2
GOWTAMI PADMA PRIYANKA PADALA <gouthamireddyppp18@gmail.com>
2025-03-15 13:32:44 +0530

    keeping linux only

--- powershellCommands.ps1
+++
@@ -1,9 +0,0 @@
-﻿#run these commands using run-selection.
-get-help  
-Get-Process
-Get-Command
-Get-Command Get-Process
-#example: Get-Command Add-AppPackage
-Stop-Process -id 7632
-Get-Service 
-stop-service "camsvc"
